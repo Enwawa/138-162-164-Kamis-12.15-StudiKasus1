e0a15b452170753fa137b760560bdf2665019ee5
Enwawa <lunglung.id@gmail.com>
2025-03-21 12:10:07 +0700

    Update tampilan ktp.cpp

--- ktp.cpp
+++ ktp.cpp
@@ -1,83 +1,101 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
-
 using namespace std;
 
-struct KTP {
+struct bio {
 	string nik;
-	string nama;
-	string tempatTglLahir;
-	string jenisKelamin;
-	string golDarah;
-	string alamat;
-	string rt_rw;
-	string kel_desa;
-	string kecamatan;
-	string agama;
-	string perkawinan;
-	string pekerjaan;
-	string kewarganegaraan;
-	string berlaku_hingga;
+    string prov;
+    string kab;
+    string nama;
+    string tempat_lahir;
+    string tgl_lahir;
+    string jenis_kelamin;
+    string goldar;
+    string alamat;
+    string rt;
+    string rw;
+    string kelurahan;
+    string kecamatan;
+    string agama;
+    string status_perkawinan;
+    string pekerjaan;
+    string kewarganegaraan;
 };
 
-void inputKTP(KTP &ktp){
-	cout << "Masukan NIK: ";
-	getline(cin, ktp.nik);
-	cout << "Masukan Nama: ";
-	getline(cin, ktp.nama);
-	cout << "Masukan Tempat/Tgl Lahir: ";
-	getline(cin, ktp.tempatTglLahir);
-	cout << "Masukan Jenis Kelamin: ";
-	getline(cin, ktp.jenisKelamin);
-	cout << "Masukan Golongan Darah: ";
-	getline(cin, ktp.golDarah);
-	cout << "Masukan Alamat: ";
-	getline(cin, ktp.alamat);
-	cout << "Masukan RT/RW: ";
-	getline(cin, ktp.rt_rw);
-	cout << "Masukan Kelurahan/Desa: ";
-	getline(cin, ktp.kel_desa);
-	cout << "Masukan Kecamatan: ";
-	getline(cin, ktp.kecamatan);
-	cout << "Masukan Agama: ";
-	getline(cin, ktp.agama);
-	cout << "Masukan Status Perkawinan: ";
-	getline(cin, ktp.perkawinan);
-	cout << "Masukan Pekerjaan: ";
-	getline(cin, ktp.pekerjaan);
-	cout << "Masukan Kewarganegaraan: ";
-	getline(cin, ktp.kewarganegaraan);
-	cout << "Masukan Berlaku Hingga: ";
-	getline(cin, ktp.berlaku_hingga);	
-}
-
-void displayKTP(const KTP &ktp){
-	cout << "\n================SIMULASI KTP==================\n";
-	cout << left << setw(20) << "NIK"<<": "<<ktp.nik<<endl;
-	cout << left << setw(20) << "Nama"<<": "<<ktp.nama<<endl;
-	cout << left << setw(20) << "Tempat/Tgl Lahir"<<": "<<ktp.tempatTglLahir<<endl;
-	cout << left << setw(20) << "Jenis Kelamin"<<": "<<ktp.jenisKelamin<<endl;
-	cout << left << setw(20) << "Golongan Darah"<<": "<<ktp.golDarah<<endl;
-	cout << left << setw(20) << "Alamat"<<": "<<ktp.alamat<<endl;
-	cout << left << setw(20) << "RT/RW"<<": "<<ktp.rt_rw<<endl;
-	cout << left << setw(20) << "Kelurahan/Desa"<<": "<<ktp.kel_desa<<endl;
-	cout << left << setw(20) << "Kecamatan"<<": "<<ktp.kecamatan<<endl;
-	cout << left << setw(20) << "Agama"<<": "<<ktp.agama<<endl;
-	cout << left << setw(20) << "Status Perkawinan"<<": "<<ktp.perkawinan<<endl;
-	cout << left << setw(20) << "Pekerjaan"<<": "<<ktp.pekerjaan<<endl;
-	cout << left << setw(20) << "Kewarganegaraan"<<": "<<ktp.kewarganegaraan<<endl;
-	cout << left << setw(20) << "Berlaku Hingga"<<": "<<ktp.berlaku_hingga<<endl;
-	cout << "=================================================";
-}
-
-int main(){
-	KTP ktp;
+int main () {
+	bio data;
 	
-	cout <<"input Data KTP\n";
-	inputKTP(ktp);
+	cout<<"Masukkan NIK Anda: ";
+	getline(cin, data.nik);
 	
-	displayKTP(ktp);
+	cout<<"Masukkan Provinsi Anda: ";
+	getline(cin, data.prov);
+	
+	cout<<"Masukkan Kabupaten Anda: ";
+	getline(cin, data.kab);
+	
+	cout<<"Masukkan Nama Anda: ";
+	getline(cin, data.nama);
+	
+	cout<<"Masukkan Tempat Lahir Anda: ";
+	getline(cin, data.tempat_lahir);
+	
+	cout<<"Masukkan Tanggal Lahir Anda: ";
+	getline(cin, data.tgl_lahir);
+	
+	cout<<"Masukkan Jenis Kelamin Anda: ";
+	getline(cin, data.jenis_kelamin);
+	
+	cout<<"Masukkan Golongan Darah Anda: ";
+	getline(cin, data.goldar);
+	
+	cout<<"Masukkan Alamat Anda: ";
+	getline(cin, data.alamat);
+	
+	cout<<"Masukkan RT Anda: ";
+	getline(cin, data.rt);
+	
+	cout<<"Masukkan RW Anda: ";
+	getline(cin, data.rw);
+	
+	cout<<"Masukkan Kelurahan Anda: ";
+	getline(cin, data.kelurahan);
+	
+	cout<<"Masukkan Kecamatan Anda: ";
+	getline(cin, data.kecamatan);
+	
+	cout<<"Masukkan Agama Anda: ";
+	getline(cin, data.agama);
+	
+	cout<<"Masukkan Status Perkawinan Anda: ";
+	getline(cin, data.status_perkawinan);
+	
+	cout<<"Masukkan Pekerjaan Anda: ";
+    getline(cin, data.pekerjaan);
+	
+	cout<<"Masukkan Kewarganegaraan Anda: ";
+	getline(cin, data.kewarganegaraan);
+	
+	cout<<endl;
+	
+	cout << "              PROVINSI  " << data.prov << endl;
+    cout << "             KABUPATEN  " << data.kab << endl;
+    cout << endl;
+    cout << "NIK                : " << data.nik << endl;
+    cout << "Nama               : " << data.nama << endl;
+    cout << "Tempat/Tgl Lahir   : " << data.tempat_lahir<< "/"<< data.tgl_lahir << endl;
+    cout << "Jenis Kelamin      : " << data.jenis_kelamin << "            Gol. Darah: " << data.goldar << endl;
+    cout << "Alamat             : " << data.alamat << endl;
+    cout << "        RT/RW              : " << data.rt << "/" << data.rw << endl;
+    cout << "        Kel/Desa           : " << data.kelurahan << endl;
+    cout << "        Kecamatan          : " << data.kecamatan << endl;
+    cout << "Agama              : " << data.agama << endl;
+    cout << "Status Perkawinan  : " << data.status_perkawinan << endl;
+    cout << "Pekerjaan          : " << data.pekerjaan << endl;
+    cout << "Kewarganegaraan    : " << data.kewarganegaraan << endl;
+    cout << "Berlaku Hingga     : Seumur Hidup" << endl;
+	
+	
 	
 	return 0;
 }
